356b9f7912201195b328a1d5bcdc029847754605
Timea Ballok <fogdokineni@gmail.com>
2022-12-09 11:11:13 +0100

    show bookmark doesn't work yet

--- src/components/Bookmark.jsx
+++ src/components/Bookmark.jsx
@@ -2,16 +2,16 @@ import React, {useState} from 'react';
 import apiFacade from "../apiFacade.js";
 import {Link} from "react-router-dom";
 
-function Bookmark({bookmarkList, fetchSingleRecipe, setErrorMessage}) {
+function Bookmark({bookmarkList, fetchSingleRecipe}) {
 
     const [recipe, setRecipe] = useState({})
     const recipeArray = []
 
     function getRecipe (id) {
-        apiFacade.fetchData("recipe/singleRecipe/" + id, (data) => {
-            console.log(data);
-            return data
-            // console.log(singleRecipe.title);
+        apiFacade.fetchData("recipe/" + id, (data) => {
+            console.log("Data" + data);
+             setRecipe(data)
+             console.log("Efter set: " + recipe);
         }, "")
     }
 
@@ -26,30 +26,38 @@ function Bookmark({bookmarkList, fetchSingleRecipe, setErrorMessage}) {
     return (
         <div className="column middle">
             <h2>Test bookmark</h2>
-            <div className="container">
-                {bookmarkList.map((bookmark) =>
-                    recipeArray.push(getRecipe(bookmark.recipeId)))
-                }
-                {recipeArray.map((recipe) => (
-                    <Link to={"/singleRecipe"} key={recipe.id}>
 
-                        <div onClick={fetchSingleRecipe} id={recipe.id} className="row">
-                            <div className="col">
-                                <img src={recipe.image}/>
-                            </div>
-                            <div className="col-6">
-                                <p>{recipe.title}</p>
-                            </div>
-                            <div className="col">
-                                3 out of 5 stars... kekw
-                            </div>
-                        </div>
-                        <br/>
-                    </Link>
+            {bookmarkList.map((bookmark) => (
+                //getRecipe(bookmark.recipeId)
+                <div>
+                <h5>{getRecipe(bookmark.recipeId)}
+                    {bookmark.recipeId}</h5>
+                <h5>{bookmark.userName}</h5>
+                </div>
+            ))
+            }
+            {/*<div className="container">*/}
+            {/*    {bookmarkList.map((bookmark) => (*/}
+            {/*        getRecipe(bookmark.recipeId)))*/}
+            {/*    }*/}
+            {/*    {recipeArray.map((data) => (*/}
+            {/*        <Link to={"/singleRecipe"} key={recipe.id}>*/}
 
-
-                ))}
-            </div>
+            {/*            <div onClick={fetchSingleRecipe} id={recipe.id} className="row">*/}
+            {/*                <div className="col">*/}
+            {/*                    <img src={data.image}/>*/}
+            {/*                </div>*/}
+            {/*                <div className="col-6">*/}
+            {/*                    <p>{recipe.title}</p>*/}
+            {/*                </div>*/}
+            {/*                <div className="col">*/}
+            {/*                    3 out of 5 stars... kekw*/}
+            {/*                </div>*/}
+            {/*            </div>*/}
+            {/*            <br/>*/}
+            {/*        </Link>*/}
+            {/*    ))}*/}
+            {/*</div>*/}
 
 
 
