72d74b438722a0b18769bcd5acda6652e3c17023
Timea Ballok <fogdokineni@gmail.com>
2022-12-13 12:40:24 +0100

    add text to Welcome page

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -4,7 +4,17 @@ const WelcomePage = () => {
   return (
     <div className="column middle">
       <div>
-        <h1>Welcome page</h1>
+        <h2>Welcome to Group X semesterproject</h2>
+          <br/><br/>
+          <h4>We have made a Food and nutrition App inspired by FN World Goal 3: Good health and well-being.</h4>
+          <br/><br/>
+          <h5>You need to sign up/login to use our functionalities.</h5>
+          <br/><br/>
+          <h5>You can search recipes and filter them. You can see diet and all nutrients on a single recipe.</h5>
+          <h5>You can bookmark your favorite recipes and you can add them to your meal plan as well.</h5>
+          <br/><br/>
+          <h5>Furthermore you can calculate your BMI</h5>
+          <br/><br/><br/>
       </div>
 
     </div>
